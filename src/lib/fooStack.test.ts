111885c1c07f349263aabbdeed75d6ec2b958e6c
Takashi Sato <takashi@tks.st>
2022-11-05 19:21:42 +0900

    source code from "cdk init" from aws-cdk@2.50.0, just modified for build

---
+++ src/lib/fooStack.test.ts
@@ -0,0 +1,19 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Foo from '../lib/fooStack.js';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/foo-stack.ts
+test("SQS Queue Created", () => {
+    //   const app = new cdk.App();
+    //     // WHEN
+    //   const stack = new Foo.FooStack(app, 'MyTestStack');
+    //     // THEN
+    //   const template = Template.fromStack(stack);
+    //   template.hasResourceProperties('AWS::SQS::Queue', {
+    //     VisibilityTimeout: 300
+    //   });
+
+    // dummy assertion for avoiding eslint warning
+    expect(true).toBe(true);
+});
